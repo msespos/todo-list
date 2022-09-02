b706c5042f2c877248eaf52164b7d6b92cdb2c42
Mike Esposito <mikeesposito6852@gmail.com>
2022-09-01 17:44:38 -0700

    Make minor changes to makeFirstTodo

--- src/todoBuilder.js
+++ src/todoBuilder.js
@@ -4,9 +4,10 @@ const Todo = (title, description, dueDate, priority, notes, checklist) => {
 
 const makeFirstTodo = () => {
   const todo1 = Todo();
-  todo1.title = "Make music";
+  todo1.title = "Make music"
   todo1.description = "Make some dang music";
-  console.log(todo1.title, todo1.description);
+  todo1.dueDate = "SOON"
+  console.log(todo1.title, todo1.description, todo1.dueDate);
 };
 
 export { makeFirstTodo }

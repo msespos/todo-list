5a001cb84433fcaa42fec5f5abfcc014d54eb638
Mike Esposito <mikeesposito6852@gmail.com>
2022-09-08 08:23:50 -0700

    Display todos and clear todos and toggle todo display

--- src/todoLayout.js
+++ src/todoLayout.js
@@ -6,13 +6,52 @@ const clearTodoTitleDisplay = (project) => {
 }
 
 const displayTodoTitles = (project) => {
-  project.forEach((todo) => {
+  project.forEach((todo, index) => {
     const div = document.getElementById("content");
     const title = document.createElement("div");
-    title.classList.add("todo-title")
+    title.classList.add("todo-title");
+    title.id = "todo-title-" + index;
     title.textContent = todo.title;
     div.appendChild(title);
+    title.onclick = () => {
+      toggleTodo(todo, index);
+    }
   });
 };
 
-export { clearTodoTitleDisplay, displayTodoTitles }
+const toggleTodo = (todo, index) => {
+  if (document.querySelector(".todo")) {
+    clearTodo();
+  } else {
+    displayTodo(todo, index);
+  }
+};
+
+const clearTodo = () => {
+  const todoElements = document.querySelectorAll(".todo");
+  todoElements.forEach((todoElement) => {
+    todoElement.remove();
+  });
+}
+
+const displayTodo = (todo, index) => {
+  const div = document.getElementById("todo-title-" + index);
+  const description = document.createElement("div");
+  description.textContent = todo.description;
+  description.classList.add("todo");
+  div.appendChild(description);
+  const dueDate = document.createElement("div");
+  dueDate.textContent = todo.dueDate;
+  dueDate.classList.add("todo");
+  div.appendChild(dueDate);
+  const priority = document.createElement("div");
+  priority.textContent = todo.priority;
+  priority.classList.add("todo");
+  div.appendChild(priority);
+  const notes = document.createElement("div");
+  notes.textContent = todo.notes;
+  notes.classList.add("todo");
+  div.appendChild(notes);
+};
+
+export { clearTodoTitleDisplay, displayTodoTitles, displayTodo }

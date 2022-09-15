a3174d08fa6ad4eedcec0aee87335bd2ae803266
Mike Esposito <mikeesposito6852@gmail.com>
2022-09-14 17:45:33 -0700

    Rename getElementByIds to specify todo

--- src/todoBuilder.js
+++ src/todoBuilder.js
@@ -9,11 +9,11 @@ let project = [];
 const makeTodo = () => {
   const makeTodoButton = document.getElementById("make-todo-button");
   makeTodoButton.onclick = () => {
-    const todo = Todo(document.getElementById("title").value,
-                      document.getElementById("description").value,
-                      document.getElementById("dueDate").value,
-                      document.getElementById("priority").value,
-                      document.getElementById("notes").value);
+    const todo = Todo(document.getElementById("todo-title").value,
+                      document.getElementById("todo-description").value,
+                      document.getElementById("todo-due-date").value,
+                      document.getElementById("todo-priority").value,
+                      document.getElementById("todo-notes").value);
     project.push(todo);
     clearTodoTitleDisplay(project);
     displayTodoTitles(project);

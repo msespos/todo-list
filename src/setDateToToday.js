f6856170e4dfdcb1a947302cf542997a565903db
Mike Esposito <mikeesposito6852@gmail.com>
2022-09-29 14:33:05 -0700

    Replace somewhat hacky solution to setDateToToday with date-fns solution

--- src/setDateToToday.js
+++ src/setDateToToday.js
@@ -1,19 +1,9 @@
-// code adapted from https://bobbyhadz.com/blog/javascript-set-current-date-to-input
+import { format } from 'date-fns'
 
 const setDateToToday = () => {
   const dateInput = document.getElementById('todo-due-date');
-  dateInput.value = formatDate();
-  dateInput.min = formatDate();
-  function formatDate(date = new Date()) {
-    return [
-      date.getFullYear(),
-      padTo2Digits(date.getMonth() + 1),
-      padTo2Digits(date.getDate()),
-    ].join('-');
-  };
-  function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  };
+  dateInput.value = format(new Date(), "yyyy-MM-dd");
+  dateInput.min = format(new Date(), "yyyy-MM-dd");
 };
 
 export { setDateToToday }

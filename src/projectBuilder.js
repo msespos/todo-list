d71f87201927f5aac9c2fb6e1d03b8a649d393cb
Mike Esposito <mikeesposito6852@gmail.com>
2022-10-09 10:47:21 -0700

    Add deleteProject and add argument to refreshProjectDisplay and export deleteProject and activeProject

--- src/projectBuilder.js
+++ src/projectBuilder.js
@@ -1,5 +1,6 @@
 import { clearProjectDisplay, displayProjects } from './projectLayout';
 import { clearTodoTitleDisplay } from './todoLayout';
+import { activeProject } from './index.js';
 
 const Project = (title, todos) => {
   return { title, todos }
@@ -11,13 +12,13 @@ const createFirstProject = () => {
   const project = Project("Default Project", []);
   projects.push(project);
   return project;
-}
+};
 
-const refreshProjectDisplay = () => {
+const refreshProjectDisplay = (projects) => {
   clearTodoTitleDisplay();
   clearProjectDisplay();
   displayProjects(projects);
-}
+};
 
 const activateCreateProjectForm = () => {
   const createProjectButton = document.getElementById("project-button");
@@ -32,9 +33,23 @@ const activateCreateProjectForm = () => {
   createProjectButton.onclick = () => {
     const project = Project(document.getElementById("project-title").value, []);
     projects.push(project);
-    refreshProjectDisplay();
+    refreshProjectDisplay(projects);
     createProjectButton.blur();
   }
 };
 
-export { projects, createFirstProject, activateCreateProjectForm, refreshProjectDisplay }
+const deleteProject = () => {
+  const projectHiddenField = document.getElementById("project-id");
+  const projectId = parseInt(projectHiddenField.value);
+  console.log("Project ID", projectId);
+  projects.splice(projectId, 1);
+  console.log("Projects", projects);
+  if (projects.length === 0) {
+    activeProject.project = createFirstProject();
+  }
+  activeProject.project = projects[0];
+  refreshProjectDisplay(projects);
+};
+
+export { projects, createFirstProject, activateCreateProjectForm, refreshProjectDisplay,
+         deleteProject, activeProject }
